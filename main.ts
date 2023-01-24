ad6f70777b7638bc0e48d3208f9de0d54ce7b9ca
ycaburto <120490571+ycaburto@users.noreply.github.com>
2023-01-24 09:37:30 -0600

    start

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 scene.setBackgroundColor(3)
+game.splash("How to make a pizza!")
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . e e e e e e e . . . . . 
+    . . . e e e e e e e e e . . . . 
+    . . e e e e e e e e e e e . . . 
+    . e e e e e e e e e e e e e . . 
+    . e e e e e e e e e e e e e . . 
+    . e e e e e e e e e e e e e . . 
+    . e e e e e e e e e e e e e . . 
+    e e e e e e e e e e e e e e . . 
+    . e e e e e e e e e e e e e . . 
+    . . e e e e e e e e e e e . . . 
+    . . . . e e e e e e e . . . . . 
+    . . . . . e e e e e . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
